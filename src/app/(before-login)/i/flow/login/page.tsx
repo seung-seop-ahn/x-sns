c334a643eeed98889a74fa901ce4a1a4a494c096
seung-seop-ahn <seungseopahn@gmail.com>
2024-05-05 16:55:48 +0900

    feat: main page

---
+++ src/app/(before-login)/i/flow/login/page.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+
+const Page = () => {
+  return <main>{`Login modal`}</main>
+}
+export default Page
---
+++ src/app/(before-login)/login/page.tsx
@@ -0,0 +1,6 @@
+import { redirect } from 'next/navigation'
+
+const Page = () => {
+  redirect('/i/flow/login')
+}
+export default Page
